9438310d8a5f4e934c28ee29df8fce6b06445cbb
Ashik <md.ashekur@gmail.com>
2025-04-09 18:41:59 +0600

    fix image

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // (optional) Set your desired output configuration
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
